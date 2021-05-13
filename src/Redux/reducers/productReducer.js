8976a8c3bb205d28a1991d3d1d16943a09797f8a
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2021-05-13 20:47:32 +0600

    Changes

--- src/Redux/reducers/productReducer.js
+++ src/Redux/reducers/productReducer.js
@@ -4,14 +4,14 @@ const initialState = {
   products: [],
 };
 
-export const productReducer = (state = initialState, { type, payload }) => {
-  switch (type) {
-    case actionTypes.SET_PRODUCTS:
-      return {
-        ...state,
-        products: payload
-      };
-    default:
-      return state;
-  }
-};
+// export const productReducer = (state = initialState, { type, payload }) => {
+//   switch (type) {
+//     case actionTypes.SET_PRODUCTS:
+//       return {
+//         ...state,
+//         products: payload
+//       };
+//     default:
+//       return state;
+//   }
+// };
